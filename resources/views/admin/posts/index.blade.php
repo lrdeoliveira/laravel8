92e3f2cb2523fb05c9c5288028f737429ecf7685
Luciano Oliveira <lrdeolivira@live.com>
2021-09-27 11:17:47 -0300

    tailwind

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,17 @@
-<h1>Posts</h1>
+<!doctype html>
+<head>
+    <!-- ... --->
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gray-500">
+<h1 class="">Posts</h1>
 @foreach($posts as $post)
-    <h1>{{$post->title}}</h1>
-    <h3>{{$post->content}}</h3>
+    <h1 class="text-white bg-gray-400" >Titulo - {{$post->title}}</h1>
+    <h3>Post - {{$post->content}}</h3>
 @endforeach
+
+
+</body>
